d3b397794b3bde0ca8845cfd43bb6125d06bf267
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-04-10 00:47:57 +0200

    update

--- Category menu/scripts/menu.js
+++ Category menu/scripts/menu.js
@@ -1,32 +1,92 @@
 let listItems = document.querySelector("#list-Items");
-let btn = document.querySelector("#all")
+let threeGridView = document.querySelector("#three-grid-view");
+let twoGridView = document.querySelector("#two-grid-view");
+let listView = document.querySelector("#list-view");
+let btnAll = document.querySelector("#all")
+let breakfast = document.querySelector("#breakfast");
+let lunch = document.querySelector("#lunch");
+let desert = document.querySelector("#desert");
+let drinks = document.querySelector("#drinks");
 var column = 3;
 let counter = 0;
+let dataJSON;
+
+let itemsArr = [];
+
+let breakfastArr = [];
+let lunchArr = [];
+let desertArr = [];
+let drinksArr = [];
+
+function printCategoryAndView() {
+    if (window.location.href.split("pages/")[1] === "menu.html") {
+        printItems(itemsArr[0].results, listItems, column);
+    } else if (window.location.href.split("pages/")[1] === "breakfast.html") {
+        printItems(breakfastArr, listItems, column);
+    } else if (window.location.href.split("pages/")[1] === "lunch.html") {
+        printItems(lunchArr, listItems, column);
+    } else if (window.location.href.split("pages/")[1] === "desert.html") {
+        printItems(desertArr, listItems, column);
+    } else if (window.location.href.split("pages/")[1] === "drinks.html") {
+        printItems(drinksArr, listItems, column);
+    } else {
+        listItems.innerHTML = "OOPS... SOMETHING WENT WRONG!"
+    }
+}
 
 $.ajax({
     url: 'https://raw.githubusercontent.com/LordGrim15/JSON-Menu/master/menu.json',
     dataType: 'json',
     success: function(data) {
-        printItems(data, listItems, column);
+        dataJSON = data
+        itemsArr.push(data);
+        filteredBreakfastArrFunction();
+        filteredLunchArrFunction();
+        filteredDesertArrFunction();
+        filtereDrinksdArrFunction();
+        printCategoryAndView();
     },
     error: function(err) {
         listItems.innerHTML = err.message;
     }
 
+});
+
+function filteredBreakfastArrFunction() {
+    breakfastArr = itemsArr[0].results.filter(function(e) {
+        return e.mainCategory === "Breakfast";
+    });
+}
+
+function filteredLunchArrFunction() {
+    lunchArr = itemsArr[0].results.filter(function(e) {
+        return e.mainCategory === "Lunch";
+    });
+}
+
+function filteredDesertArrFunction() {
+    desertArr = itemsArr[0].results.filter(function(e) {
+        return e.mainCategory === "Desert";
+    });
+}
+
+function filtereDrinksdArrFunction() {
+    drinksArr = itemsArr[0].results.filter(function(e) {
+        return e.mainCategory === "Drink";
+    });
+}
+
+threeGridView.addEventListener("click", function() {
+    column = 3;
+    printCategoryAndView();
 })
-
-btn.addEventListener("click", function() {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/LordGrim15/JSON-Menu/master/menu.json',
-        dataType: 'json',
-        success: function(data) {
-            printItems(data, listItems, column);
-        },
-        error: function(err) {
-            listItems.innerHTML = err.message;
-        }
-
-    })
+twoGridView.addEventListener("click", function() {
+    column = 2;
+    printCategoryAndView();
+})
+listView.addEventListener("click", function() {
+    column = 1;
+    printCategoryAndView();
 })
 
 function counterFunc() {
@@ -35,45 +95,43 @@ function counterFunc() {
 }
 
 function printItems(data, htmlItem, column) {
+    counter = 0;
     htmlItem.innerHTML = ``;
-    for (let i = 0; i < data.results.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         htmlItem.innerHTML += `
             <div class="row list-item" id="row${counterFunc()}">
                 <div class="col-sm block-click">
-                    <img src="${data.results[i].itemImg}" width="250px" height="200px" alt="${data.results[i].name}" />
+                    <img src="${data[i].itemImg}" width="250px" height="200px" alt="${data[i].name}" />
                     <ul class="list-items">
-                    <li>${data.results[i].itemName}</li>
-                    <li>${data.results[i].mainCategory}</li>
-                    <li>${data.results[i].secondCategory}</li>
-                    <li>${data.results[i].price} мкд</li>
+                    <li>${data[i].itemName}</li>
+                    <li>${data[i].mainCategory}</li>
+                    <li class="price-class">${data[i].price} мкд</li>
                     </ul>
                 </div>
              </div>
             `;
         let row = `row${counter}`
         let rowz = document.getElementById(row);
-        if (data.results[i + 1] != null) {
+        if (data[i + 1] != null) {
             if (column === 2 || column === 3) {
                 rowz.innerHTML += `<div class="col-sm block-click">
-                <img src="${data.results[i+1].itemImg}" width="250px" height="200px" alt="${data.results[i+1].name}" />
+                <img src="${data[i+1].itemImg}" width="250px" height="200px" alt="${data[i+1].name}" />
                 <ul class="list-items">
-                <li>${data.results[i+1].itemName}</li>
-                <li>${data.results[i+1].mainCategory}</li>
-                <li>${data.results[i+1].secondCategory}</li>
-                <li>${data.results[i+1].price} мкд</li>
+                <li>${data[i+1].itemName}</li>
+                <li>${data[i+1].mainCategory}</li>
+                <li class="price-class">${data[i+1].price} мкд</li>
                 </ul>
             </div>`
             }
         }
-        if (data.results[i + 2] != null) {
+        if (data[i + 2] != null) {
             if (column === 3) {
                 rowz.innerHTML += `<div class="col-sm block-click">
-            <img src="${data.results[i+2].itemImg}" width="250px" height="200px" alt="${data.results[i+1].name}" />
+            <img src="${data[i+2].itemImg}" width="250px" height="200px" alt="${data[i+1].name}" />
             <ul class="list-items">
-            <li>${data.results[i+2].itemName}</li>
-            <li>${data.results[i+2].mainCategory}</li>
-            <li>${data.results[i+2].secondCategory}</li>
-            <li>${data.results[i+2].price} мкд</li>
+            <li>${data[i+2].itemName}</li>
+            <li>${data[i+2].mainCategory}</li>
+            <li class="price-class">${data[i+2].price} мкд</li>
             </ul>
             </div>`
             }
