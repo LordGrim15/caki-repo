2f9a00b42a65bf8825d4501a9983f047e69f7e80
Cakizy <58572124+Cakizy@users.noreply.github.com>
2020-04-09 14:37:54 +0200

    Last update 4.9.2020
    Last update 4.9.2020

--- Category menu/scripts/menu.js
+++ Category menu/scripts/menu.js
@@ -1,26 +1,25 @@
-let listItems = document.querySelector("#list-items");
+let listItems = document.querySelector("#list-Items");
 let btn = document.querySelector("#all")
+var column = 3;
+
 $.ajax({
     url: 'https://raw.githubusercontent.com/LordGrim15/JSON-Menu/master/menu.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data);
-        printItems(data, listItems);
-        console.log(data.results);
+        printItems(data, listItems, column);
     },
     error: function(err) {
         listItems.innerHTML = err.message;
     }
 
 })
+
 btn.addEventListener("click", function() {
     $.ajax({
         url: 'https://raw.githubusercontent.com/LordGrim15/JSON-Menu/master/menu.json',
         dataType: 'json',
         success: function(data) {
-            console.log(data);
-            printItems(data, listItems);
-            console.log(data.results);
+            printItems(data, listItems, column);
         },
         error: function(err) {
             listItems.innerHTML = err.message;
@@ -29,23 +28,83 @@ btn.addEventListener("click", function() {
     })
 })
 
-function printItems(data, htmlItem) {
-    for (let i = 0; i < data.results.length; i++) {
-
+function printItems(data, htmlItem, column) {
+    htmlItem.innerHTML = ``;
+    if (column === 1) {
+        for (const item of data.results) {
+            htmlItem.innerHTML += `
+            <div class="row">
+            <div class="col-sm">
+                <img src="${item.itemImg}" width="250px" height="200px" alt="${item.name}" />
+                <ul>
+                <li>${item.itemName}</li>
+                <li>${item.mainCategory}</li>
+                <li>${item.secondCategory}</li>
+                <li>${item.price}</li>
+                </ul>
+            </div>
+            </div>`
+        }
+    } else if (column === 2) {
+        for (let i = 0; i < data.results.length; i++) {
+            htmlItem.innerHTML += `
+        <div class="row">
+            <div class="col-sm">
+                <img src="${data.results[i].itemImg}" width="250px" height="200px" alt="${data.results[i].name}" />
+                <ul>
+                <li>${data.results[i].itemName}</li>
+                <li>${data.results[i].mainCategory}</li>
+                <li>${data.results[i].secondCategory}</li>
+                <li>${data.results[i].price}</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <img src="${data.results[i+1].itemImg}" width="250px" height="200px" alt="${data.results[i+1].name}" />
+                <ul>
+                <li>${data.results[i+1].itemName}</li>
+                <li>${data.results[i+1].mainCategory}</li>
+                <li>${data.results[i+1].secondCategory}</li>
+                <li>${data.results[i+1].price}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+            i += 1
+        }
+    } else {
+        for (let i = 0; i < data.results.length; i++) {
+            htmlItem.innerHTML += `
+        <div class="row">
+            <div class="col-sm">
+                <img src="${data.results[i].itemImg}" width="250px" height="200px" alt="${data.results[i].name}" />
+                <ul>
+                <li>${data.results[i].itemName}</li>
+                <li>${data.results[i].mainCategory}</li>
+                <li>${data.results[i].secondCategory}</li>
+                <li>${data.results[i].price}</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <img src="${data.results[i+1].itemImg}" width="250px" height="200px" alt="${data.results[i+1].name}" />
+                <ul>
+                <li>${data.results[i+1].itemName}</li>
+                <li>${data.results[i+1].mainCategory}</li>
+                <li>${data.results[i+1].secondCategory}</li>
+                <li>${data.results[i+1].price}</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <img src="${data.results[i+2].itemImg}" width="250px" height="200px" alt="${data.results[i+2].name}" />
+                <ul>
+                <li>${data.results[i+2].itemName}</li>
+                <li>${data.results[i+2].mainCategory}</li>
+                <li>${data.results[i+2].secondCategory}</li>
+                <li>${data.results[i+2].price}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+            i += 2
+        }
     }
-    // for (const item of data.results) {
-    //     htmlItem.innerHTML += `
-    //     <li><img src="${item.itemImg}" width="250px" height="200px" alt="${item.name}" /></li>
-    //     <li>${item.itemName}</li>
-    //     <li>${item.mainCategory}</li>
-    //     <li>${item.secondCategory ? item.secondCategory : "No Second Category"}</li>
-    //     <li>${item.price}</li>
-    //     `
-    // }
 }
-
-
-
-document.addEventListener("load", function() {
-
-})
